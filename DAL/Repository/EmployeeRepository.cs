9de18b696cfec75a8f461b80eb937c497fd89836
brookd-lab <54964060+brookd-lab@users.noreply.github.com>
2023-01-12 17:54:13 -0800

    Initial Checkin
    All projects (3)

---
+++ DAL/Repository/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Data;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        async Task<IEnumerable<Employee>> IEmployeeRepository.GetAllEmployees()
+        {
+            var employeeList = await _context.Employee.ToListAsync();
+            return employeeList;
+        }
+        async Task<Employee> IEmployeeRepository.GetEmployeeById(int id)
+        {
+            Employee? employee = await _context.Employee.FindAsync(id);
+            return employee;
+        }
+
+        async Task IEmployeeRepository.CreateEmployee(Employee employee)
+        {
+            await _context.Employee.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        async Task IEmployeeRepository.UpdateEmployeeById(int id, Employee employee)
+        {
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        async Task IEmployeeRepository.DeleteEmployeeById(int id)
+        {
+            var employeeToDelete = await _context.Employee.FindAsync(id);
+            if (employeeToDelete != null)
+            {
+                _context.Remove(employeeToDelete);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ DAL/Repository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+
+namespace DAL.Repository
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllEmployees();
+        Task<Employee> GetEmployeeById(int id);
+        Task CreateEmployee(Employee employee);
+        Task UpdateEmployeeById(int id, Employee employee);
+        Task DeleteEmployeeById(int id);
+    }
+}
